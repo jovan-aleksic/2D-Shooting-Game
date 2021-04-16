e8fd53a4902b6d151ecb244c59284c405c079cd5
spider1123 <albertsmith0309@gmail.com>
2021-04-16 02:32:24 -0400

    PhaseII: Core Programming: New Enemy Movement: Move Direction Switch Based on Game Move Direction.

--- Assets/Scripts/Ships/Enemy.cs
+++ Assets/Scripts/Ships/Enemy.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class Enemy : Ship
@@ -46,25 +47,33 @@ public class Enemy : Ship
         switch (gameMoveDirectionVariable.Value)
         {
             case GameMoveDirectionEnum.TopToBottom:
+            {
                 if (position.y < bounds.Min.y)
                     transform.position =
                         PositionHelper.GetRandomPosition(gameMoveDirectionVariable.Value, bounds.Value);
                 break;
+            }
             case GameMoveDirectionEnum.BottomToTop:
+            {
                 if (position.y > bounds.Max.y)
                     transform.position =
                         PositionHelper.GetRandomPosition(gameMoveDirectionVariable.Value, bounds.Value);
                 break;
+            }
             case GameMoveDirectionEnum.LeftToRight:
+            {
                 if (position.x > bounds.Max.x)
                     transform.position =
                         PositionHelper.GetRandomPosition(gameMoveDirectionVariable.Value, bounds.Value);
                 break;
+            }
             case GameMoveDirectionEnum.RightToLeft:
+            {
                 if (position.x < bounds.Min.x)
                     transform.position =
                         PositionHelper.GetRandomPosition(gameMoveDirectionVariable.Value, bounds.Value);
                 break;
+            }
         }
     }
 
@@ -76,6 +85,21 @@ public class Enemy : Ship
         float x = m_moveDirection.Value.x;
         float y = m_moveDirection.Value.y;
         float z = m_moveDirection.Value.z;
+
+        //x = Mathf.Cos(Time.time);
+
+        switch (gameMoveDirectionVariable.Value)
+        {
+            case GameMoveDirectionEnum.TopToBottom:
+            case GameMoveDirectionEnum.BottomToTop:
+                x = Mathf.Cos(Time.time);
+                break;
+            case GameMoveDirectionEnum.LeftToRight:
+            case GameMoveDirectionEnum.RightToLeft:
+                y = Mathf.Sin(Time.time);
+                break;
+        }
+
 
         m_moveDirection.Value = new Vector3(x, y, z);
     }
