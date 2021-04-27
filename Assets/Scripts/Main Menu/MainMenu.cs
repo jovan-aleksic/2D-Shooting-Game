5d96ae13b3216e103f35ab770ce16f5a11544988
spider1123 <albertsmith0309@gmail.com>
2021-04-27 03:30:48 -0400

    Add exit to Main Menu.

--- Assets/Scripts/Main Menu/MainMenu.cs
+++ Assets/Scripts/Main Menu/MainMenu.cs
@@ -7,4 +7,15 @@ public class MainMenu : MonoBehaviour
     {
         SceneManager.LoadScene(sceneBuildIndex: 1);
     }
+
+    public void ExitGame()
+    {
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #elif UNITY_WEBGL
+                Application.OpenURL("https://jameslafritz.com/");
+        #else
+                Application.Quit();
+        #endif
+    }
 }
