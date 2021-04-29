40afd9834ebdbbba90cd71026527367f8bf64aa1
spider1123 <albertsmith0309@gmail.com>
2021-04-29 07:43:17 -0400

    Added A To do list class with todo items to help keep track of what i want to add or change.

---
+++ Assets/Scripts/Editor/ToDo.cs
@@ -0,0 +1,59 @@
+#region Description
+// ToDo.cs
+// 04-29-2021
+// James LaFritz
+#endregion
+
+public class ToDo
+{
+    /*
+     * ToDo: Attributes for inspector
+     *          ToDo: ● Header with Separator, Color, Size
+     *          ToDo: ● Scene Selection Attribute
+     *          ToDo: ● Tag Selection Attribute
+     *          ToDo: ● Input Selection Attribute "For the Player"
+     *          ToDo: ● Info Box Attribute
+     * 
+     * ToDo: Remove the Laser Firing from Ship, Create a new Gun Prefab that can be added that controls the laser.
+     * ToDo: Add Total retrys for a level before having to start over at the beginig.
+     * ToDo: Add Destroy All Objects Pick Up.
+     * ToDo: New Enemy Movement
+     *          ToDo: ● Enable the Enemies to move in a new way,either from side to side, circling, or coming into the play field at an angle.
+     *              Random Movement Added.
+     * Player Ammo
+     *          ● Visualize on screenthe ammo count of the player in the form of current/max
+     * Wave System
+     *          ● Implement wave sequencing of enemies with more enemies coming each wave.
+     * Negative Pickup
+     *          ● Create a powerup that negatively affects the player.
+     * ToDo: New Enemy Type
+     *          ● Create enemy types that can fire &damage the player
+     *          ToDo: ● Create enemy type with unique projectile.(ie. Laser beam, heatseeking, etc)
+     *          ToDo: ● Unique MovementBehavior (zig-zag,wave, etc)
+     * Balanced Spawning
+     *          ● Create a balancedspawning systembetween Enemies &pickups
+     *          ● Ie. Pickups like Health should be rare, where ammo is frequent
+     * ToDo: Enemy Shields
+     *          ● Provide logic for some enemies to have shields
+     *          ● Shields allow the enemies to take 1 free hit.
+     * ToDO: Aggressive Enemy Type
+     *          ToDO: ● Create the functionality to support enemy aggression
+     *          ToDO: ● If an enemy is close to a player, the enemy will try and “ram” it.
+     * ToDo: Smart Enemy
+     *          ToDO ● Create an enemy type that knows when it’s behind the player, and fires a weapon backwards.
+     * ToDO: Enemy Pickups
+     *          ToDO: ● If a pickup is in front of an enemy, the enemy will fire its weapon at the pickup to destroy it before the player can get it.
+     * ToDO: Pickup Collect
+     *          ToDO: ●When the ‘C’ key is pressed by the Player,Pickups quickly moveto the player.
+     * ToDO: Enemy Avoid Shot
+     *          ToDO: ● Create an enemy type that can avoid the player’s weapon
+     *          ToDO: ● When you fire a shot,the enemy should detect a shot in range and try to avoid it.
+     * Homing Projectile (Completed in Phase I.
+     *          ● Create a homing projectile that seeks the closest target.
+     *          ● Turn into a rare powerup
+     * ToDO: Boss AI
+     *          ToDO: ● Create a final wave that includes a boss at the end.
+     *          ToDO: ● Moves down the screen to the center and stays there.
+     *          ToDO: ● Unique attacks towards the player
+     */
+}
