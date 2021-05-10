f33dc084a87208a06bd3e7c92a101cbfcc6912d8
spider1123 <albertsmith0309@gmail.com>
2021-05-09 22:47:36 -0400

    Win screen added. Packages Updated. Waves Modified to include all newly added enemies.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,45 +1,50 @@
 using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using Debug = System.Diagnostics.Debug;
 
 public class GameManager : MonoBehaviour
 {
-    [Scene] [SerializeField] private string mainMenuScene;
+    [Scene] [SerializeField] private string m_mainMenuScene;
 
-    [SerializeField] private CodedGameEventListener waveSpawnerComplete;
-    [Scene] [SerializeField] private int spawnerCompleteSceneToLoad;
+    [SerializeField] private CodedGameEventListener m_waveSpawnerComplete;
+    [Scene] [SerializeField] private int m_spawnerCompleteSceneToLoad;
 
-    [SerializeField] private IntReference score;
+    [SerializeField] private IntReference m_score;
     private int m_savedScore;
 
-    [SerializeField] private CodedGameEventListener playerDestroyed;
+    [SerializeField] private CodedGameEventListener m_playerDestroyed;
 
-    [SerializeField] private GameOverController gameOverController;
+    [SerializeField] private GameOverController m_gameOverController;
+
+    [SerializeField] private Text m_winText;
 
     private bool m_gameOver;
 
     private void OnDisable()
     {
-        waveSpawnerComplete?.OnDisable();
-        playerDestroyed?.OnDisable();
+        m_waveSpawnerComplete?.OnDisable();
+        m_playerDestroyed?.OnDisable();
     }
 
     private void OnEnable()
     {
-        waveSpawnerComplete?.OnEnable(WaveSpawnerCompleted);
-        playerDestroyed?.OnEnable(OnPlayerDestroyed);
+        m_waveSpawnerComplete?.OnEnable(WaveSpawnerCompleted);
+        m_playerDestroyed?.OnEnable(OnPlayerDestroyed);
     }
 
     private void Awake()
     {
-        m_savedScore = score.Value;
+        Debug.Assert(m_score != null, nameof(m_score) + " != null");
+        m_savedScore = m_score.Value;
     }
 
     private void Update()
     {
         if (Input.GetButtonDown("Exit Game"))
         {
-            SceneManager.LoadScene(mainMenuScene);
+            SceneManager.LoadScene(m_mainMenuScene);
         }
 
         if (m_gameOver && Input.GetButtonDown("Restart Level"))
@@ -55,20 +60,22 @@ public class GameManager : MonoBehaviour
 
     private IEnumerator WaveSpawnerCompleteRoutine()
     {
+        if (m_winText is { }) m_winText.gameObject.SetActive(true);
         yield return new WaitForSeconds(2);
-        SceneManager.LoadScene(spawnerCompleteSceneToLoad);
+        SceneManager.LoadScene(m_spawnerCompleteSceneToLoad);
     }
 
     private void OnPlayerDestroyed()
     {
-        if (gameOverController != null)
-            gameOverController.DisplayGameOver();
+        if (m_gameOverController != null)
+            m_gameOverController.DisplayGameOver();
         m_gameOver = true;
     }
 
     private void RestartLevel()
     {
-        score.Value = m_savedScore;
+        Debug.Assert(m_score != null, nameof(m_score) + " != null");
+        m_score.Value = m_savedScore;
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
